4120e31a504b3a7ca41c753da6c806384db55e6b
kopal <kopalloves28@gmail.com>
2019-01-25 00:11:37 +0530

    Updates

---
+++ precache-manifest.0f4a9db073d3d50bd20fa07d681b5692.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "c41aacbd6d1bc73f40fd",
+    "url": "/linkdin-app/static/js/runtime~main.c41aacbd.js"
+  },
+  {
+    "revision": "bf25d3a91a88a2625b5f",
+    "url": "/linkdin-app/static/js/main.bf25d3a9.chunk.js"
+  },
+  {
+    "revision": "4a34e45d34cafb7623bd",
+    "url": "/linkdin-app/static/js/1.4a34e45d.chunk.js"
+  },
+  {
+    "revision": "bf25d3a91a88a2625b5f",
+    "url": "/linkdin-app/static/css/main.6f3bf701.chunk.css"
+  },
+  {
+    "revision": "1c9c28c5c2ca19a8b0ecd0630b983ebc",
+    "url": "/linkdin-app/index.html"
+  }
+];
